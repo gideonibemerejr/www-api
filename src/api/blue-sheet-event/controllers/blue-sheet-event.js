2de5b61c0fdc441a2ede548017193dc1f019b875
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2022-02-21 02:54:32 -0600

    chore no real update

--- src/api/blue-sheet-event/controllers/blue-sheet-event.js
+++ src/api/blue-sheet-event/controllers/blue-sheet-event.js
@@ -6,23 +6,4 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController(
-  "api::blue-sheet-event.blue-sheet-event",
-  ({ strapi }) => ({
-    async create(ctx) {
-      // some custom logic here
-
-      // Calling the default core action
-      const response = await super.create(ctx);
-
-      return response;
-    },
-    async update(ctx) {
-      // some logic here
-      const response = await super.update(ctx);
-      // some more logic
-
-      return response;
-    },
-  })
-);
+module.exports = createCoreController("api::blue-sheet-event.blue-sheet-event");
--- src/api/blue-sheet-event/services/blue-sheet-event.js
+++ src/api/blue-sheet-event/services/blue-sheet-event.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * blue-sheet-event service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::blue-sheet-event.blue-sheet-event');
+module.exports = createCoreService("api::blue-sheet-event.blue-sheet-event");
