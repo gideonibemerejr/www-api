74f380b97e57988564bfc6de39c6b19211de8766
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2023-03-01 14:02:17 -0600

    fix: migration issues

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '6571af19cf709348030ef701eac20d0a'),
+    secret: env('ADMIN_JWT_SECRET'),
+    apiToken: {
+      salt: env('API_TOKEN_SALT'),
+    },
   },
 });
--- config/plugins.js
+++ config/plugins.js
@@ -24,4 +24,9 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
 });
