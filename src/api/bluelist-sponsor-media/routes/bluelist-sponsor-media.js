2996e5a135acd03dd1739e0c7eb15fca35df028a
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2023-03-07 21:40:46 -0600

    feat: Adds bluelist sponsor image stuff

---
+++ src/api/bluelist-sponsor-media/routes/bluelist-sponsor-media.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bluelist-sponsor-media router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bluelist-sponsor-media.bluelist-sponsor-media');
---
+++ src/api/bluelist-sponsor-media/services/bluelist-sponsor-media.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bluelist-sponsor-media service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::bluelist-sponsor-media.bluelist-sponsor-media');
