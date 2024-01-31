69975c2ce57c380ce16b3480fcff16ee42393018
Gideon Ibemere, Jr <gibemere@hiddenlayer.com>
2024-01-30 22:33:38 -0600

    feat: Adds bluelist item to backend

---
+++ src/api/bluelist-item/routes/bluelist-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bluelist-item router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bluelist-item.bluelist-item');
---
+++ src/api/bluelist/controllers/bluelist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bluelist controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::bluelist.bluelist');
